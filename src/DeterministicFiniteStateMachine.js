de81ede8715adb1c908727797561c56ec05f060b
Rachit <rachit.nc@gmail.com>
2020-02-12 18:16:11 -0800

    Update DeterministicFiniteStateMachine.js

--- src/DeterministicFiniteStateMachine.js
+++ src/DeterministicFiniteStateMachine.js
@@ -3,7 +3,9 @@ export default class DeterministicFiniteStateMachine {
   /**
    */
   constructor({ transitions, startState, acceptStates }) {
-    throw 'IMPLEMENT constructor(description)';
+    this._transitions = transitions;
+    this._startState  = startState;
+    this._acceptStates = acceptStates;
   }
 
   /**
@@ -11,11 +13,25 @@ export default class DeterministicFiniteStateMachine {
    * @returns a string state name
    */
   transition(state, symbol) {
-    throw 'IMPLEMENT transition(state, symbol)';
+    return this._transitions[state][symbol];
   }
 
   accepts(string, state = this._startState) {
-    throw 'IMPLEMENT FiniteStateMachine.accepts';
+    var index;
+    var current_state = state;
+    var current_token = string.charAt(0);
+    for (index = 0; index < (string.length); index++)
+    {
+      current_token = string.charAt(index);
+      current_state = this._transitions[current_state][current_token];
+    }
+    if (this._acceptStates == current_state)
+    {
+      return true
+    }
+    else {
+      return false
+    }
   }
 
 }
